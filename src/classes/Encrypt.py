fc5a0ca8326f3ade55f633fcd49f7a02598c1c8b
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-08-04 11:07:39 +0200

    added rot47

--- src/classes/Encrypt.py
+++ src/classes/Encrypt.py
@@ -12,7 +12,24 @@ class Encrypt(ABC):
         options.get(mode)()
 
     def rot13(self):
-        print('Selected ROT13! Current text:', self.buffer.get_buffer())
+        print('Selected ROT13!')
+        print('Currently encrypting', self.buffer.get_buffer())
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        encrypted_text = "".join([alphabet[(alphabet.find(char) + 13) % 26] for char in self.buffer.get_buffer()])
+        self.buffer.set_buffer(encrypted_text)
+        print('Encrypted successfully!')
+        print('Encrypted text:', encrypted_text)
 
     def rot47(self):
-        print('Selected ROT47! Current text:', self.buffer.get_buffer())
+        print('Selected ROT47!')
+        print('Currently encrypting', self.buffer.get_buffer())
+        x = []
+        for i in range(len(self.buffer.get_buffer())):
+            j = ord(self.buffer.get_buffer()[i])
+            if j >= 33 and j <= 126:
+                x.append(chr(33 + ((j + 14) % 94)))
+            else:
+                x.append(self.buffer.get_buffer()[i])
+        encrypted_text = ''.join(x)
+        print('Encrypted successfully!')
+        print('Encrypted text:', encrypted_text)
